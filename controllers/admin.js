45ac23fea9a255ecad8ef18aee69ccc9468e2285
Shubh0622 <shubhambachhav8@gmail.com>
2024-04-23 10:11:00 +0530

    relation between user and product added successfully

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user
+    .createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
     description: description
-  })
+    })
     .then(result => {
       console.log('Created Product');
       res.redirect('/admin/products');
@@ -32,8 +33,13 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then((product)=>{
+  req.user.getProducts({where: {id: prodId}})
+  // Product.findByPk(prodId)
+    .then((products)=>{
+      const product = products[0];
+      if(!product){
+        return res.redirect('/');
+      }
       res.render('admin/edit-product', {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
@@ -78,7 +84,8 @@ exports.postDeleteProduct =(req,res,next)=>{
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then(products =>{
       res.render('admin/products', {
         prods: products,
